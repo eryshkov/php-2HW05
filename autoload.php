8871cc342570ffdaa1771b65741fd6705eace2f4
Evgenii Ryshkov <eryshkov@gmail.com>
2019-03-27 20:25:23 +0300

    3rd task is started

--- autoload.php
+++ autoload.php
@@ -1,9 +1,18 @@
 <?php
 
+/**
+ * @param $class
+ * @throws \App\Exceptions\FileNotExistException
+ */
 function autoload($class)
 {
     $file = str_replace('\\', '/', $class) . '.php';
-    require __DIR__ . '/' . $file;
+    $fullFilePath = __DIR__ . '/' . $file;
+    if (file_exists($fullFilePath)) {
+        require $fullFilePath;
+    } else {
+        throw new \App\Exceptions\FileNotExistException();
+    }
 }
 
 spl_autoload_register('autoload');
