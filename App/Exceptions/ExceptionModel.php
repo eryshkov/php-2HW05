9e1709b64295a3079053b5bf8cdb27f3930c4690
Evgenii Ryshkov <eryshkov@gmail.com>
2019-04-01 21:42:13 +0300

    ExceptionModel.php is fixed

--- App/Exceptions/ExceptionModel.php
+++ App/Exceptions/ExceptionModel.php
@@ -6,15 +6,4 @@ use App\Logger;
 
 abstract class ExceptionModel extends \Exception
 {
-    /**
-     * ExceptionModel constructor.
-     * @param string $message
-     * @param int $code
-     * @param \Exception $previous
-     */
-    public function __construct(string $message = '', int $code = 0, \Exception $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-        Logger::log($this);
-    }
 }
