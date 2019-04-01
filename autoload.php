9be1c7b264b2e7b58be7547444df048eaae5a1c2
Evgenii Ryshkov <eryshkov@gmail.com>
2019-04-01 16:33:48 +0300

    autoload.php is fixed

--- autoload.php
+++ autoload.php
@@ -2,17 +2,11 @@
 
 /**
  * @param $class
- * @throws \App\Exceptions\FileNotExistException
  */
 function autoload($class)
 {
     $file = str_replace('\\', '/', $class) . '.php';
-    $fullFilePath = __DIR__ . '/' . $file;
-    if (file_exists($fullFilePath)) {
-        require $fullFilePath;
-    } else {
-        throw new \App\Exceptions\FileNotExistException($fullFilePath);
-    }
+    require __DIR__ . '/' . $file;
 }
 
 spl_autoload_register('autoload');
